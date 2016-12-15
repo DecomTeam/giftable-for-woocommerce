0b3ef62c9e44e81e61f7506a4353272852bae521
damirdecom <damir@decom.ba>
2016-12-15 12:07:14 +0100

    Use remove_all_actions for removing product summary hooks

--- public/templates/giftable-variations.php
+++ public/templates/giftable-variations.php
@@ -14,13 +14,6 @@
 // no tumbnails
 remove_action( 'woocommerce_product_thumbnails', 'woocommerce_show_product_thumbnails', 20 );
 
-// clear all after_single_product_summary hooks
-// we do it this way because there's no way to know what's added by custom themes
-global $wp_filter;
-if (isset($wp_filter['woocommerce_after_single_product_summary'])) {
-    $wp_filter['woocommerce_after_single_product_summary'] = array();
-}
-
 /**
  * Product Summary Box.
  *
@@ -34,7 +27,7 @@ if (isset($wp_filter['woocommerce_after_single_product_summary'])) {
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
-
+remove_all_actions('woocommerce_after_single_product_summary');
 ?>
 <div class="dgfw-product-giftable-variations product" id="dgfw-product-<?php echo $product->id; ?>-variations">
     <?php wc_get_template_part( 'content', 'single-product' ); ?>
